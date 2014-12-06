822decbdf92020d38a25784c06df20e289fc01b7
priyam10 <patelpriyam1011@gmail.com>
2014-12-05 19:55:50 -0500

    Update Gui.java
    Fixed progress bar issues. Added a table that displays peers and their info such as IP, port, choked status.

--- src/Gui.java
+++ src/Gui.java
@@ -1,30 +1,58 @@
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.awt.*;
 import javax.swing.*;
 
 
+
 public class Gui extends JFrame{
 	private Timer timer;
 	private RUBTClient client;
-	private JButton exit;
 	JProgressBar progbar;
-
+	JTable table;
+	JPanel panel;
+	JLabel label;
 	public Gui(RUBTClient rubt){
-		super("RUBTClient");
+		setTitle("RUBTClient");
+		panel = new JPanel();
+		panel.setPreferredSize(new Dimension(300, 100));
+		getContentPane().add(panel);	
 		this.client = rubt;
-		setLayout(new FlowLayout());
 		//Progress bar set up
+		label = new JLabel("Download Progress...");
+		label.setPreferredSize(new Dimension(280, 24));
+		panel.add(label);
 		progbar = new JProgressBar();
+		progbar.setPreferredSize( new Dimension( 300, 20 ) );
 		progbar.setMaximum(100);
 		progbar.setMinimum(0);
-		progbar.setStringPainted(true);
 		progbar.setValue(this.client.getProgressBarPercent());
-		this.setContentPane(progbar);
-		this.exit = new JButton("EXIT");
-		add(this.exit);
-		
-
+		progbar.setStringPainted(true);
+		progbar.setBounds(20, 35, 260, 20);
+		panel.add(progbar);
+		String[] columnNames = {"Peer IP", "Port", "Choked By Client?"};
+		ArrayList<Peer> peers = client.peers;
+		int peer_index = 0;
+		Object[][] data = new Object[peers.size()][3];
+		for(int row=0; row < peers.size(); row++){
+			for(int col=0; col < 3; col++){
+				switch(col){
+					case 0:
+						data[row][col] = peers.get(peer_index).peerIp;
+						break;
+					case 1:
+						data[row][col] = new Integer(6881);
+						break;
+					case 2:
+						data[row][col] = peers.get(peer_index).amChoking;
+						break;
+				}
+			}
+			peer_index++;
+		}
+		table = new JTable(data, columnNames);
+		panel.add(new JScrollPane(this.table));
 		//Make a timer for progress bar updates
 		this.timer = new Timer(true);
 		this.timer.scheduleAtFixedRate(new TimerTask(){
@@ -39,5 +67,6 @@ public class Gui extends JFrame{
 		
 		
 	}
+
 
 }
