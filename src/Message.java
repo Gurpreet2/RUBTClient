2a67a5e08e139047b2a12cfe34101a18d7bbca4f
Gurpreet2 <gurpreet.pannu@live.com>
2014-12-07 18:27:46 -0500

    Update Message.java
    Support for saving a piece after its downloaded and hash-checked.
    
    Minor Fixes.

--- src/Message.java
+++ src/Message.java
@@ -102,7 +102,11 @@ public class Message {
 	 * @param length - length of the incoming message
 	 * @throws IOException 
 	 */
-	public static void dealWithMessage(Peer peer) throws IOException {
+	public synchronized static void dealWithMessage(Peer peer) throws IOException {
+		
+		// If there's nothing waiting to be read, return
+		if (peer.socketInput.available() == 0) return;
+		
 		//logger.info("Reading message from peer : " + peer.peerId);
 		DataInputStream dis = peer.socketInput;
 		// Read the amount of bytes incoming
@@ -111,28 +115,25 @@ public class Message {
 			System.out.println("Invalid length " + length);
 			return; // Got the 131081 from the Message.java class that was uploaded in Sakai. I also
 			// understand its importance (messages can have a maximum length of 131081 bytes... at least for now).
-
+			
 		}
-		
 		// Peer sent a seemingly valid message so reset the peer timer (to keep the connection from timing out)
 		peer.peerLastMessage = System.currentTimeMillis();
 		
 		// Message is just a keep_alive message
 		if (length == 0) {
 			//logger.info("Received {KEEP_ALIVE} message from Peer ID : [ " + peer.peerId + " ].");
-			//System.out.println("Received {KEEP_ALIVE} message from Peer ID : [ " + peer.peerId + " ].");
 			return;
 		}
 		
 		// Message is not a keep alive message, get prepared for it
-		byte[] bite = new byte[1]; //using byte array here because for some reason doesn't work well if using readByte()
 		int piece_index, block_offset, block_length, piece_length = peer.client.torrentInfo.piece_length;
 		byte[] block;
 		try {
 			// Read the first byte in the message and deal with the message appropriately
 			
 			int type = dis.readByte();
-
+			
 			if(type < 0 || type > 8){
 				System.out.println("Unknown byte index: " + type);
 				System.exit(1);
@@ -140,77 +141,87 @@ public class Message {
 			//System.out.println("Byte: " + type);
 			//logger.info("Received {" + message_types[type] +"} message from Peer ID : [ " + peer.peerId + " ].");
 			if(type != 4){
-				System.out.println("Received {" + message_types[type] +"} message from [ " + peer.peerIp + " ].");
+				System.out.println("Received {" + message_types[type] +"} message from [ "
+						+ "ID : [ " + peer.peerId + "] with IP : [ " + peer.peerIp
+						+ " ].");
 			}
-
+			
 			switch (type) {
 				case 0:
 					peer.peerChoking = true;
-					MyTools.saveDownloadedPieces(peer.client);
 					break;
 				case 1:
 					peer.peerChoking = false;
+					// When the peer choked, he dropped all of the requests, so have to resend them
+					while (!peer.requestedQueue.isEmpty()) {
+						peer.requestSendQueue.put(peer.requestedQueue.take());
+					}
 					break;
 				case 2:
 					peer.peerInterested = true;
+					peer.sendMessage(createUnchoke());
 					break;
 				case 3:
 					peer.peerInterested = false;
+					if (!peer.amInterested || peer.client.amSeeding)
+						peer.shutdown();
 					break;
 				case 4:
 					//Message is a have message
-					if (!peer.sentGreetings) {
-			        	peer.sendMessage(Message.createUnchoke());
-			        	peer.sendMessage(Message.createInterested());
-			        	peer.sentGreetings = true;
-					}
 					//First, read the piece_index of the have message
 					piece_index = dis.readInt();
 					//Set value of the peerhavearray to true
 					peer.peerHaveArray[piece_index] = true;
 					//create a new request message to get the piece if client doesn't have it
-					if (!peer.client.havePieces[piece_index])
-						peer.sendMessage(createRequest(piece_index, 0, peer.client.torrentInfo.piece_length));
-					//If the peer is a bad torrent controller, then need to check all of have messages to make it a seed
-					if (peer.peerHaveArray.length == peer.client.numOfPieces) {
-						if(!peer.isSeed) peer.isSeed = true;
+					if (!peer.client.havePieces[piece_index]) {
+						// Send interested and unchoke since its a piece you want
+						if (!peer.sentGreetings) {
+				        	peer.sendMessage(createInterested());
+				        	peer.sendMessage(createUnchoke());
+				        	peer.sentGreetings = true;
+						}
+						peer.wantFromPeer++;
+						if (piece_index == peer.client.numOfPieces - 1)
+							peer.requestSendQueue.put(createRequest(piece_index, 0, peer.client.torrentInfo.file_length % piece_length));
+						else
+							peer.requestSendQueue.put(createRequest(piece_index, 0, peer.client.torrentInfo.piece_length));
 					}
 					break;
 				case 5:
 					//Message is a bitfield message (and therefore the peer is not a bad torrent controller)
 					peer.badTorrentController = false;
-					//Send unchoke and interested
-					if (!peer.sentGreetings) {
-					    peer.sendMessage(Message.createUnchoke());
-					    peer.sendMessage(Message.createInterested());
-					    peer.sentGreetings = true;
-					}
 					//Store the bitfield in the peer
 					byte[] peerBitfield = new byte[length - 1];
-					dis.read(peerBitfield);
-					peer.peerBytefield = new boolean[peer.client.numOfPieces];
-					int count = 0;
-					for (int i = 0; i < peer.peerBytefield.length; i++) {
-						if (MyTools.isBitSet(peerBitfield, i)) {
-							peer.peerBytefield[i] = true;
-							count++;
+					dis.readFully(peerBitfield);
+					peer.peerBitfield = peerBitfield;
+					for (int i = 0; i < peer.client.numOfPieces; i++) {
+						if (MyTools.isBitSet(peerBitfield, i) && !peer.client.havePieces[i]) {
+							//Send unchoke and interested if peer has something I want
+							if (!peer.sentGreetings) {
+							    peer.sendMessage(createInterested());
+							    peer.sendMessage(createUnchoke());
+							    peer.sentGreetings = true;
+							}
+							peer.wantFromPeer++;
+							if (i == peer.client.numOfPieces - 1)
+								peer.requestSendQueue.put(createRequest(i, 0, peer.client.torrentInfo.file_length % piece_length));
+							else
+								peer.requestSendQueue.put(createRequest(i, 0, peer.client.torrentInfo.piece_length));
 						}
 					}
-					System.out.println(peer.peerIp + "'s bitfield: " + Arrays.toString(peer.peerBytefield));
-					peer.sizeOfBytefield = count;
 					break;
 				case 6:
 					//Message is a request message (if have it, send it)
 					piece_index = dis.readInt();
 					block_offset = dis.readInt();
 					block_length = dis.readInt();
+					// If choking, ignore the message
+					if (peer.amChoking) break;
 					if (peer.client.havePieces[piece_index]) {
 						byte[] payload = new byte[block_length];
-						payload = Arrays.copyOfRange(peer.client.rawFileBytes, 
-								(piece_index * piece_length) + block_offset, 
-								(piece_index + 1) * piece_length);
-						peer.sendMessage(createPiece(piece_index, block_offset, payload));
-						peer.client.bytesUploaded = peer.client.bytesUploaded + payload.length;
+						peer.client.theDownloadFile.seek(piece_index * piece_length);
+						peer.client.theDownloadFile.read(payload, 0, payload.length);
+						peer.pieceSendQueue.offer(createPiece(piece_index, block_offset, payload));
 					}
 					break;
 				case 7:
@@ -221,7 +232,7 @@ public class Message {
 					dis.readFully(block);
 					//System.out.println("PIECE BLOCK: " + piece_index + " " + block_offset + " " + Arrays.toString(block));
 					//System.exit(1);
-					//peer.client.addToReceiveQueue(createPiece(piece_index, block_offset, block));
+					// Verify the piece message's hash
 					MessageDigest md = MessageDigest.getInstance("SHA");
 					byte[] hash = null;
 					hash = md.digest(block);
@@ -230,33 +241,22 @@ public class Message {
 					for (int i = 0; i < checkWith.length; i++)
 						if (checkWith[i] != hash[i]) cray = false;
 					if (cray) {
-						for (int i = (piece_index * piece_length) + block_offset; 
-								i < (piece_index * piece_length) + block_offset + block.length; i++) {
-							peer.client.rawFileBytes[i] = block[i-(piece_index * piece_length + block_offset)];
-						}
-						peer.client.bytesLeft = peer.client.bytesLeft - length + 9;
-						peer.client.bytesDownloaded = peer.client.bytesDownloaded + length - 9;
+						peer.client.theDownloadFile.seek(piece_index * piece_length);
+						peer.client.theDownloadFile.write(block, 0, block.length);
+						peer.client.bytesLeft -= block.length;
+						peer.client.bytesDownloaded += block.length;
 						peer.client.havePieces[piece_index] = true;
 						peer.client.numOfHavePieces++;
 						peer.downloadedFromPeer++;
-						peer.client.haveSendQueue.offer(createHave(piece_index), 2500, TimeUnit.MILLISECONDS);
+						peer.requestsSent--;
+						peer.requestedQueue.remove(createRequest(piece_index, block_offset, block.length));
+						peer.pieces_received++; // For throttle
+						for (Peer peer1 : peer.client.neighboring_peers)
+							peer1.haveSendQueue.offer(createHave(piece_index), 2500, TimeUnit.MILLISECONDS);
 					} else {
-						peer.sendMessage(createRequest(piece_index, block_offset, length));
+						peer.requestSendQueue.put(createRequest(piece_index, block_offset, length));
 					}
 					break;
-				case 8:
-					// Message is a cancel message
-					piece_index = dis.readInt();
-					block_offset = dis.readInt();
-					block_length = dis.readInt();
-					// TODO
-					/*byte[] payload = new byte[block_length];
-					payload = Arrays.copyOfRange(
-							peer.rawFileBytes, 
-							(piece_index * peer.client.torrent_info.piece_length) + block_offset, 
-							(piece_index + 1) * peer.client.torrent_info.piece_length);
-					peer.removeFromSendQueue(createPiece(piece_index, block_offset, payload));
-					break;*/
 				default:
 					break;
 			}
@@ -334,7 +334,8 @@ public class Message {
 	 * @return
 	 */
 	public static Message createInterested() {
-		return new Message((byte) 2);
+		Message message = new Message((byte) 2);
+		return message;
 	}
 	
 	
